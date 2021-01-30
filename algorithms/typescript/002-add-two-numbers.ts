5cf2e35d955eb56aaba231cb71fedc1426e89ae6
kanoe <kanoemon.cho@gmail.com>
2021-01-30 21:11:48 +0900

    Update 002-add-two-numbers.ts

--- algorithms/typescript/002-add-two-numbers.ts
+++ algorithms/typescript/002-add-two-numbers.ts
@@ -11,17 +11,40 @@
  */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    const sum: number = Number(toArray(l1, []).reverse().join('')) 
-                + Number(toArray(l2, []).reverse().join(''));
-    const sumArray: string[] = sum.toString().split('');
+    const l1Array: number[] = toArray(l1, []);
+    const l2Array: number[] = toArray(l2, []);
+    const largerArray: number[] = l1Array.length >= l2Array.length ? l1Array : l2Array;
+    const smallArray: number[] = l1Array.length < l2Array.length ? l1Array : l2Array;
     
+    let sumResult = [];
+    let numToAdd: number = 0;
+    let sum = 0;
+    for (let i = 0; i < largerArray.length; i++) {
+        if (smallArray[i] === undefined) {
+            sum = largerArray[i] + numToAdd;
+        } else {
+            sum = largerArray[i] + smallArray[i] + numToAdd;
+        }
+        
+        const sumForResult = sum.toString().length === 1 ? sum : Number(sum.toString().slice(-1));
+        
+        numToAdd = sum.toString().length === 1 ? 0 : 1;
+
+        sumResult.push(sumForResult);
+    }
+    
+    if (numToAdd) sumResult.push(1);
+            
     let result: ListNode | null = null;
     let beforeListNode: ListNode | null = null;
-    for(let i: number = 0; i < sumArray.length; i++) {
-        result = beforeListNode = new ListNode(Number(sumArray[i]), beforeListNode);
+    
+    sumResult.reverse();
+    for(let i: number = 0; i < sumResult.length; i++) {
+        result = beforeListNode = new ListNode(Number(sumResult[i]), beforeListNode);
     }
     return result;
 };
+
 
 function toArray(target: ListNode | null, result: number[]): number[] {
     result.push(target.val);
