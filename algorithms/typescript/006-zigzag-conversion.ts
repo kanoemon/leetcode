b922aa1add5d8c606221265fe749a6c5771dc27a
kanoe <kanoemon.cho@gmail.com>
2021-02-05 22:57:00 +0900

    Update 006-zigzag-conversion.ts

--- algorithms/typescript/006-zigzag-conversion.ts
+++ algorithms/typescript/006-zigzag-conversion.ts
@@ -2,34 +2,24 @@ function convert(s: string, numRows: number): string {
     
     let convertRows = [];
     let p: number = 0;
-    let r: number = numRows - 1;
+    let lastRow: number = numRows - 1;
+    let r: number = lastRow;
     
     while(p < s.length) { 
-        if (r === 0) r = numRows - 1;
+        if (r === 0) r = lastRow;
         
         for(let i: number = 0; i < numRows; i++) {
-            if (convertRows[i] === undefined) convertRows[i] = [];
+            if (convertRows[i] === undefined) convertRows[i] = '';
             
-            let v: string = (s[p] === undefined) ? '' : s[p];
+            if (s[p] === undefined) break;
 
-            if (r === 0 || r === numRows - 1) {
-                convertRows[i].push(v);
+            if (r === lastRow || r === i) {
+                convertRows[i] += s[p];
                 p++;
-            } else if (i === r) {
-                convertRows[i].push(v);
-                p++;
-            } else {
-                convertRows[i].push('');
             }
         }
         r--;
     }
     
-    let rs: string = '';
-    for(let j: number = 0; j < convertRows.length; j++) {
-        //rs += convertRows[j].join('');
-    }
-
-    return rs;
-
+    return convertRows.join('');
 };
